b877095152e6a2e0a5c4f809f089bb1bf5378be2
kssrao13882 <kssrao13882@gmail.com>
2024-01-12 09:28:50 +0530

    Revert "marble: Use 6GB dalvik-heap config"
    This reverts commit 72f89526b060a2edd35d5db143eb1552a648cb8e.

--- device.mk
+++ device.mk
@@ -13,9 +13,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
 # Call the proprietary setup.
 $(call inherit-product, vendor/xiaomi/marble/marble-vendor.mk)
 
