f257186f09957699ea65cda4c56901dc542d0579
markakash <akashniki@gmail.com>
2024-01-21 06:25:07 +0530

    marble: Use 8Gb dalvik heap size config
     - Helps fix java.lang.OutOfMemoryError: Failed to allocate a 24 byte allocation with 2582544 free bytes and 2522KB until OOM, target footprint 268435456, growth limit 268435456; giving up on allocation because <1% of heap free after GC
    
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
+
 # Call the proprietary setup.
 $(call inherit-product, vendor/xiaomi/marble/marble-vendor.mk)
 
